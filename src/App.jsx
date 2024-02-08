3de75b6578d5089f8d6bc08c107e0b12406a122b
sunghankwon <akvo852@naver.com>
2024-02-09 00:59:36 +0900

    feat: Implement functionality in the extension to retrieve a token

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="font-bold m-80">hello</div>;
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const token = new URLSearchParams(window.location.search).get("token");
+    async function verifyToken() {
+      if (!token) {
+        return;
+      }
+
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL,
+        { token: token },
+        { withCredentials: true },
+      );
+
+      const user = res.data.user;
+
+      setUserData(user);
+    }
+
+    verifyToken();
+  }, []);
+
+  return (
+    <div className="font-bold m-80">
+      {userData ? `${userData.nickname}님이 로그인 하셨습니다.` : "not Login"}
+    </div>
+  );
 }
 
 export default App;
