cd8b24f8bcfe30892a52236e50d582249343e498
sunghankwon <akvo852@naver.com>
2024-02-10 21:29:12 +0900

    feat: Add loading and login failure screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [userData, setUserData] = useState(null);
+  const [loginCheck, setLoginCheck] = useState("loading");
 
   useEffect(() => {
     const token = new URLSearchParams(window.location.search).get("token");
     async function verifyToken() {
       if (!token) {
-        return;
+        setLoginCheck("fail");
       }
       try {
         const res = await axios.post(
@@ -20,13 +21,27 @@ function App() {
         const user = res.data.user;
 
         setUserData(user);
+        setLoginCheck("success");
       } catch (error) {
         console.log("Login error:", error);
+        setLoginCheck("fail");
       }
     }
 
     verifyToken();
   }, []);
+
+  if (loginCheck === "loading") {
+    return <div className="font-bold m-80">로딩중입니다...</div>;
+  }
+
+  if (loginCheck === "fail") {
+    return (
+      <div className="font-bold m-8 text-red-500">
+        로그인에 실패하였습니다...
+      </div>
+    );
+  }
 
   return (
     <div className="font-bold m-80">
