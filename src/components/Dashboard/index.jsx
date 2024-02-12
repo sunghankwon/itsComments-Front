d35129a0fddaa85fbe0d8ae7ec8deba6550c53a8
sunghankwon <akvo852@naver.com>
2024-02-13 01:35:21 +0900

    feat: Create Dashboard component for displaying user comments

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,16 @@
+function Dashboard() {
+  return (
+    <div className="w-full h-full">
+      <button className="ml-[100px] mr-1 text-black text-2xl font-bold py-2 px-4 rounded">
+        My Comments
+      </button>
+      <span className="mr-1 text-black text-2xl font-bold py-2 px-4 rounded">
+        |
+      </span>
+      <button className="mr-1 text-black text-2xl font-bold py-2 px-4 rounded">
+        Received Comments
+      </button>
+    </div>
+  );
+}
+export default Dashboard;
