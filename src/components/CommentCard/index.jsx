7edac2ea251694c561303fe07bcdff5270173ba1
sunghankwon <akvo852@naver.com>
2024-02-13 01:36:41 +0900

    feat: Implement logic to display user comments in Dashboard component and create CommentCard component

---
+++ src/components/CommentCard/index.jsx
@@ -0,0 +1,43 @@
+export default function CommentCard({ data }) {
+  const commentCreator = data.creator.nickname;
+  const commentText = data.text;
+  const commentPostDate = new Date(data.postDate);
+  const commentScreenshot = isValidBase64(data.screenshot);
+  const formattedDate = formatDate(commentPostDate);
+
+  return (
+    <div className="w-[150px] h-[220px] mt-4 ml-4 border rounded-md overflow-hidden shadow-md">
+      <div className="p-2 bg-black text-center text-[#38D431] font-bold text-sm">
+        {commentCreator}
+      </div>
+      <div className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+        {commentText}
+      </div>
+      <img
+        className="w-full h-[130px] object-cover border-t"
+        src={`${commentScreenshot}`}
+        alt="Comment Screenshot"
+      />
+      <div className="p-2 text-gray-500 text-xs">{formattedDate}</div>
+    </div>
+  );
+}
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
+
+function isValidBase64(str) {
+  try {
+    return atob(str);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
