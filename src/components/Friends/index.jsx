1ffaa79b2bae2e6b5c7a46bf570058ff8c43cb6f
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-04-15 13:07:01 +0900

    test: FriendCard component test

--- src/components/Friends/index.jsx
+++ src/components/Friends/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import useUserStore from "../../store/useUser";
 import useFriendsStore from "../../store/useFriends";
