15651b85b0ee05cc4f54e7ae8980f315b045c2e4
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-04-15 13:12:08 +0900

    test: import React for test

--- src/components/Modal/FriendDelete.jsx
+++ src/components/Modal/FriendDelete.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import useUserStore from "../../store/useUser";
 import useFriendsStore from "../../store/useFriends";
